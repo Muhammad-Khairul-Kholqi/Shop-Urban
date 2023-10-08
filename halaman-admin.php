2852623b5841ad2f4ff0ff483c963f44d34a32ae
Muhammad-Khairul-Kholqi <khairulkhlqi31@gmail.com>
2023-10-08 20:47:59 +0700

    Menambah kodingan dan style halaman admin

---
+++ halaman-admin.php
@@ -0,0 +1,47 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Shop Urban Outfitters</title>
+        <link rel="website icon" href="img/logoshop.png" type="png">
+        <link rel="stylesheet" href="css/styleadmin.css">
+        <!-- link bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="admin-page">
+            <div class="sidebar">
+                <div class="header">
+                    <img src="img/logoshop.png" alt="">
+                    <p><span class="span-title">Shop</span><br>Urban Outfitters</p>
+                </div>
+                <div class="menu">
+                    <div class="menu-link">
+                        <a href=""><img src="img/dash.png" alt=""></a>
+                        <a href=""><p>Dashboard</p></a>
+                    </div>
+                    <div class="menu-link">
+                        <a href=""><img src="img/folder.png" alt=""></a>
+                        <a href=""><p>Categories</p></a>
+                    </div>
+                    <div class="menu-link">
+                        <a href=""><img src="img/list.png" alt=""></a>
+                        <a href=""><p class="title-link">Orders</p></a>
+                    </div>
+                </div>
+                <div class="tombol-keluar">
+                    <center>
+                    <a href="">
+                        <img src="img/logout.png">
+                        <p>LogOut</p>
+                    </a>
+                    </center>
+                </div>
+            </div>
+            <div class="main-content">
+                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum neque repudiandae sed nihil tenetur libero impedit deleniti ipsa ex voluptates iste, sunt quae beatae iusto harum. Minus explicabo, totam enim fuga aliquam quidem. Molestias, ratione enim autem in eligendi iste. Fugit quia assumenda culpa ducimus magni at officiis est natus rerum sint a ratione id, iste repellendus blanditiis voluptas ipsa minus soluta? Reiciendis cupiditate, consectetur ipsam vel hic dicta voluptates ducimus! Debitis minus architecto doloribus, earum maiores voluptate, ipsum voluptates in placeat rerum reiciendis dolores nemo accusamus delectus totam voluptatem eligendi asperiores tempora quia cum impedit cupiditate natus odio. In?</h1>
+            </div>
+        </div>
+    </body>
+    </html>
